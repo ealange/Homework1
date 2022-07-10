c228ccc4ebff5490f3fdbf3315f06b52253a5b66
Eugen Lange <lange.e@mail.ru>
2022-07-10 10:07:36 +0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        out.println("Hello world!");
     }
 }
